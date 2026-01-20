202d4ab63c97f9c83524c0a2b348fb11360282b5
Tamer Omar <tameraamr@gmail.com>
2026-01-20 20:52:24 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -591,7 +591,7 @@ const getFilteredChartData = () => {
 </div>
 {/* --- نهاية كود الأزرار --- */}                            
                             <ResponsiveContainer width="100%" height="100%">
-                              <AreaChart data={getFilteredChartData()}>
+                              <AreaChart data={getFilteredChartData()} key={timeRange}>
                                 <defs>
                                   <linearGradient id="colorPrice" x1="0" y1="0" x2="0" y2="1">
                                     <stop offset="5%" stopColor="#3b82f6" stopOpacity={0.4}/>
